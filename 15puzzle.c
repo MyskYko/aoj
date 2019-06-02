8bb9f7792ed61c09ad8b1a95703e8e2182a827fc
miyasaka <yoyuohlhjl@yahoo.co.jp>
2019-06-02 14:54:19 +0900

    15puzzle

--- 15puzzle.c
+++ 15puzzle.c
@@ -1,155 +1,361 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
-typedef struct node{
-  int *data;
-  int depth;
-  struct node *next;
-}Node;
+#define HEAP_SIZE 5000000
+#define HASH_SIZE 10000000
+#define MAX_STEPS 45
 
-Node *create_Node(int *data, int depth) {
-  Node *p = (Node*)malloc(sizeof(Node));
-  p->data = data;
-  p->depth = depth;
-  p->next = NULL;
-  return p;
+typedef struct _node {
+	unsigned long long data;
+	int steps;
+	int f;
+	struct _node * next;
+} node;
+
+unsigned int hash(unsigned long long data) {
+	unsigned long long hash = 0;
+	hash += data * 9966091;
+	return (unsigned int)(hash % HASH_SIZE);
 }
 
-Node *pop(Node *begin) {
-  Node *tmp = begin->next;
-  free(begin->data);
-  free(begin);
-  return tmp;
+node * hash_unique_create(node ** hash_table, unsigned long long data, int steps, int f) {
+	unsigned int hash_value = hash(data);
+	//std::printf("%016llx, %x, %d\n", data, hash_value, steps);
+	node * p = hash_table[hash_value];
+	if (p == NULL) {
+		node *q = (node*)malloc(sizeof(node));
+		q->data = data;
+		q->steps = steps;
+		q->f = f;
+		q->next = NULL;
+		hash_table[hash_value] = q;
+		return q;
+	}
+	if (p->data == data) {
+		if (p->steps > steps) {
+			p->f = f;
+			p->steps = steps;
+			return p;
+		}
+		return NULL;
+	}
+	while (p->next != NULL) {
+		p = p->next;
+		if (p->data == data) {
+			if (p->steps > steps) {
+				p->f = f;
+				p->steps = steps;
+				return p;
+			}
+			return NULL;
+		}
+	}
+
+	node *q = (node*)malloc(sizeof(node));
+	q->data = data;
+	q->steps = steps;
+	q->f = f;
+	q->next = NULL;
+	p->next = q;
+	return q;
 }
 
-Node *push_back(Node *begin, Node *p) {
-  p->next = NULL;
-  
-  if(begin == NULL) {
-    return p;
-  }
-  else {
-  Node *tmp = begin;
-  while(tmp->next != NULL) {tmp = tmp->next;}
-  tmp->next = p;
-  return begin;
-  }
-  return NULL;
+int Mdistance(unsigned long long data) {
+	int distance[16];
+	for (int j = 1; j < 16; j++) {
+		distance[j] = 0;
+		for (int i = 0; i < 16; i++) {
+			unsigned int a;
+			a = data >> (60 - 4 * i);
+			a = a % 16;
+
+			if ((int)a == j) {
+				distance[j] = 0;
+				distance[j] += std::abs(i % 4 - (j - 1) % 4);
+				distance[j] += std::abs(i / 4 - (j - 1) / 4);
+			}
+		}
+	}
+
+	int sum = 0;
+	for (int j = 1; j < 16; j++) {
+		sum += distance[j];
+	}
+	return sum;
 }
 
-//nomatch = 0, match = 1;
-int compare_data(int *data1, int *data2) {
-  int i;
-  for(i = 0; i < 16; i++) {
-    if(data1[i] != data2[i]) {return 0;}
-  }
-  return 1;
+void heap_add(node ** heap, int * n, node * a) {
+	*n += 1;
+	int tmp_n = *n;
+	while (tmp_n != 1) {
+		if (heap[tmp_n / 2]->f > a->f) {
+			heap[tmp_n] = heap[tmp_n / 2];
+			tmp_n = tmp_n / 2;
+		}
+		else {
+			break;
+		}
+	}
+	heap[tmp_n] = a;
 }
 
-//not found = -1, found = depth
-int search_for(int *data, Node *begin) {
-  Node *tmp = begin;
-  while(tmp != NULL) {
-    if(compare_data(data, tmp->data)) {return tmp->depth;}
-    tmp = tmp->next;
-  }
-  return -1;
+void heap_delete(node ** heap, int * n, int i) {
+	if (i == 1) {
+		heap[1] = heap[*n];
+		*n -= 1;
+	}
+
+	if (2 * i > *n) {
+		return;
+	}
+	if (2 * i + 1 > *n) {
+		if (heap[i]->f > heap[2 * i]->f) {
+			node * tmp = heap[i];
+			heap[i] = heap[2 * i];
+			heap[2 * i] = tmp;
+		}
+		return;
+	}
+
+	if (heap[i]->f < heap[2 * i]->f && heap[i]->f < heap[2 * i + 1]->f) {
+		return;
+	}
+
+	if (heap[2 * i]->f < heap[2 * i + 1]->f) {
+		node * tmp = heap[i];
+		heap[i] = heap[2 * i];
+		heap[2 * i] = tmp;
+		heap_delete(heap, n, 2 * i);
+	}
+	else {
+		node * tmp = heap[i];
+		heap[i] = heap[2 * i + 1];
+		heap[2 * i + 1] = tmp;
+		heap_delete(heap, n, 2 * i + 1);
+	}
 }
 
-void add_Node(Node* n){
-  int i;
-  int addr;
-  for(i = 0; i < 16; i++) {
-    if(n->data[i] == 0) {
-      addr = i;
-      break;
-    }
-  }
-  
-  //moveup
-  if(addr > 3) {
-    int *up_data = (int*)malloc(16*sizeof(int));
-    for(i = 0; i < 16; i++) {
-      up_data[i] = n->data[i];
-    }
-    up_data[addr-4] = 0;
-    up_data[addr] = n->data[addr-4];
-    Node *up = create_Node(up_data, n->depth+1);
-    push_back(n,up);
-  }
-  //movedown
-  if(addr < 12) {
-    int *down_data = (int*)malloc(sizeof(int)*16);
-    for(i = 0; i < 16; i++) {
-      down_data[i] = n->data[i];
-    }
-    down_data[addr+4] = 0;
-    down_data[addr] = n->data[addr+4];
-    Node *down = create_Node(down_data, n->depth+1);
-    push_back(n,down);
-  }
-  //moveleft
-  if(addr%4 != 0) {
-    int *left_data = (int*)malloc(sizeof(int)*16);
-    for(i = 0; i < 16; i++) {
-      left_data[i] = n->data[i];
-    }
-    left_data[addr-1] = 0;
-    left_data[addr] = n->data[addr-1];
-    Node *left = create_Node(left_data, n->depth+1);
-    push_back(n,left);
-  }
-  //moveright
-  if(addr%4 != 3) {
-    int *right_data = (int*)malloc(sizeof(int)*16);
-    for(i = 0; i < 16; i++) {
-      right_data[i] = n->data[i];
-    }
-    right_data[addr+1] = 0;
-    right_data[addr] = n->data[addr+1];
-    Node *right = create_Node(right_data, n->depth+1);
-    push_back(n,right);
-  }
-  
+/*
+At the end, return 1; otherwise, return 0;
+*/
+int create(unsigned long long data, int steps, int f,
+	node ** hash_table, node ** heap, int * n)
+{
+	node * p = hash_unique_create(hash_table, data, steps, f);
+	if (p == NULL) {
+		return 0;
+	}
+
+	if (data == 0x123456789abcdef0) {
+		return 1;
+	}
+
+	if (steps == MAX_STEPS) {
+		return 0;
+	}
+
+	heap_add(heap, n, p);
+	if (*n > HEAP_SIZE) {
+		printf("heap is full\n");
+		throw 1;
+	}
+
+	return 0;
 }
 
-Node* create_list() {
-  int i;
-  int *first = (int*)malloc(16*sizeof(int));
-  for(i = 0; i < 15; i++) {first[i] = i+1;}
-  first[15] = 0;
-  Node *begin = create_Node(first, 0);
-  Node *finished = NULL;
-  
-  while(begin != NULL) {
-    if(search_for(begin->data, finished) >= 0) {
-      begin = pop(begin);
-    }
-    else if(begin->depth >= 11){
-      Node *tmp = begin;
-      begin = begin->next;
-      finished = push_back(finished, tmp);
-    }
-    else{
-      add_Node(begin);
-      Node *tmp = begin;
-      begin = begin->next;
-      finished = push_back(finished, tmp);
-    }
-  }
-  
-  return finished;
+int find_zero(unsigned long long data) {
+	for (int i = 0; i < 16; i++) {
+		unsigned int a;
+		a = data >> (60 - 4 * i);
+		a = a % 16;
+
+		if (a == 0) {
+			return i;
+		}
+	}
+	return 0;
 }
 
-int main(int argc, char **argv) {
-  Node *begin = create_list();
-  int i;
-  int *data = (int*)malloc(sizeof(int)*16);
-  printf("please type the data\n");
-  for(i = 0; i < 16; i++) {
-    scanf("%d", data+i);
-  }
-  int depth = search_for(data, begin);
-  printf("%d\n", depth);
-  return 0;
+// zero does move.
+
+unsigned long long move_right(unsigned long long data, int zero_index, int * dif_f) {
+	unsigned long long a;
+	a = (data >> (60 - 4 * (zero_index + 1))) % 16;
+	if (((int)a - 1) % 4 < (zero_index + 1) % 4) {
+		*dif_f = 0;
+	}
+	else {
+		*dif_f = 2;
+	}
+	a = a << (60 - 4 * (zero_index + 1));
+	unsigned long long b;
+	b = data - a;
+	a = a << 4;
+	b += a;
+
+	return b;
+}
+
+unsigned long long move_left(unsigned long long data, int zero_index, int * dif_f) {
+	unsigned long long a;
+	a = (data >> (60 - 4 * (zero_index - 1))) % 16;
+	if (((int)a - 1) % 4 > (zero_index - 1) % 4) {
+		*dif_f = 0;
+	}
+	else {
+		*dif_f = 2;
+	}
+	a = a << (60 - 4 * (zero_index - 1));
+	unsigned long long b;
+	b = data - a;
+	a = a >> 4;
+	b += a;
+
+	return b;
+}
+
+unsigned long long move_down(unsigned long long data, int zero_index, int * dif_f) {
+	unsigned long long a;
+	a = (data >> (60 - 4 * (zero_index + 4))) % 16;
+	if (((int)a - 1) / 4 < (zero_index + 4) / 4) {
+		*dif_f = 0;
+	}
+	else {
+		*dif_f = 2;
+	}
+	a = a << (60 - 4 * (zero_index + 4));
+	unsigned long long b;
+	b = data - a;
+	a = a << 16;
+	b += a;
+
+	return b;
+}
+
+unsigned long long move_up(unsigned long long data, int zero_index, int * dif_f) {
+	unsigned long long a;
+	a = (data >> (60 - 4 * (zero_index - 4))) % 16;
+	if (((int)a - 1) / 4 > (zero_index - 4) / 4) {
+		*dif_f = 0;
+	}
+	else {
+		*dif_f = 2;
+	}
+	a = a << (60 - 4 * (zero_index - 4));
+	unsigned long long b;
+	b = data - a;
+	a = a >> 16;
+	b += a;
+
+	return b;
+}
+
+int main() {
+	unsigned long long data;
+	//unsigned long long goal = 0x123456789abcdef0;
+	node ** heap;
+	int n;
+	node ** hash_table;
+	int finished;
+	int result;
+
+	heap = (node **)malloc(sizeof(node *) * HEAP_SIZE + 1);
+	for (int i = 1; i <= HEAP_SIZE; i++) {
+		heap[i] = NULL;
+	}
+	n = 0;
+	hash_table = (node **)malloc(sizeof(node *) * HASH_SIZE);
+	for (int i = 0; i < HASH_SIZE; i++) {
+		hash_table[i] = NULL;
+	}
+
+	data = 0;
+	for (int i = 0; i < 16; i++) {
+		unsigned int a;
+		std::cin >> a;
+		data = (data << 4) + a;
+	}
+
+	try
+	{
+		finished = create(data, 0, Mdistance(data), hash_table, heap, &n);
+	}
+	catch (...)
+	{
+		return 1;
+	}
+	if (finished) {
+		std::printf("0\n");
+		return 0;
+	}
+
+	finished = 0;
+	while (1) {
+		if (n == 0) {
+			std::printf("empty heap\n");
+			return 1;
+		}
+
+		node * p = heap[1];
+		heap_delete(heap, &n, 1);
+
+		unsigned long long new_data;
+		int zero_index = find_zero(p->data);
+		int dif_f = 0;
+
+		if (zero_index % 4 != 3) {
+			new_data = move_right(p->data, zero_index, &dif_f);
+			try
+			{
+				finished += create(new_data, p->steps + 1, p->f + dif_f, hash_table, heap, &n);
+			}
+			catch (...)
+			{
+				return 1;
+			}
+		}
+		if (zero_index % 4 != 0) {
+			new_data = move_left(p->data, zero_index, &dif_f);
+			try
+			{
+				finished += create(new_data, p->steps + 1, p->f + dif_f, hash_table, heap, &n);
+			}
+			catch (...)
+			{
+				return 1;
+			}
+		}
+		if (zero_index < 12) {
+			new_data = move_down(p->data, zero_index, &dif_f);
+			try
+			{
+				finished += create(new_data, p->steps + 1, p->f + dif_f, hash_table, heap, &n);
+			}
+			catch (...)
+			{
+				return 1;
+			}
+		}
+		if (zero_index > 3) {
+			new_data = move_up(p->data, zero_index, &dif_f);
+			try
+			{
+				finished += create(new_data, p->steps + 1, p->f + dif_f, hash_table, heap, &n);
+			}
+			catch (...)
+			{
+				return 1;
+			}
+		}
+
+		if (finished) {
+			result = p->steps + 1;
+			break;
+		}
+	}
+
+	std::printf("%d\n", result);
+
+	return 0;
 }
